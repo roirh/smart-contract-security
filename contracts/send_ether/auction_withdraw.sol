36b67f996a3b0795ba9a300917572e51cb7b662a
roi <roi.rodriguez.huertas@gmail.com>
2023-01-05 16:48:37 +0100

    license header

--- contracts/send_ether/auction_withdraw.sol
+++ contracts/send_ether/auction_withdraw.sol
@@ -1,5 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+/* This file is part of https://github.com/roirh/smart-contract-security.
+ * Copyright (c) 2023 Roi Rodriguez.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+// SPDX-License-Identifier: GPL-3.0
 import "../utils/owned.sol";
 
 pragma solidity >=0.7.0 <0.9.0;
